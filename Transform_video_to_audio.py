55058fa4bf9d320ef408e432abb4990bb450bf96
Xinghua Tao <xhtao@pku.edu.cn>
2018-11-09 13:19:23 -0500

    convert video to audio

---
+++ Transform_video_to_audio.py
@@ -0,0 +1,17 @@
+FFMPEG_BIN = "C:/ffmpeg/bin/ffmpeg.exe"
+import subprocess as sp
+
+# define a new function and its parameters are the path of input and output materials
+def convert_video(video_input, video_output):
+    command = [FFMPEG_BIN,
+               "-i", video_input,  # input path
+               "-f", "mp3",  # format
+               "-vn",  # disable video recording
+               "-ar", "44100",  # sets the sampling rate for audio streams
+               "-ac", "2",  # set the number of audio channels
+               "-b:a", "1640k",  # set the audio bit
+               video_output]  # output path
+    sp.Popen(command)
+
+# test new function
+convert_video("C:/Users/taotao/Desktop/research/test.mp4", "C:/Users/taotao/Desktop/research/test.mp3")
