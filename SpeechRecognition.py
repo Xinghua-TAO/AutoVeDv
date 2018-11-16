3ca5bc3b77d855c7029f515c5e8fcd67299a9814
TAO <44677512+Xinghua-TAO@users.noreply.github.com>
2018-11-16 12:53:18 -0500

    catch error

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -1,15 +1,21 @@
 # use the package speech_recognition
 import speech_recognition as sr
 
-# define the function and the parameter audio_input means the path of the audio and the txt_output means the path and the name of your output file
+# define the function and the parameter audio_input means the path of the audio
+# and the txt_output means the path and the name of your output file
+
 def recognize_speech(audio_input,txt_output):
     r = sr.Recognizer()
     beginner = sr.AudioFile(audio_input)
     with beginner as source:
         audio = r.record(source)
+
+    try:
         # use Google Web Speech API
+        print("Google Web Speech results:")
         txt = r.recognize_google(audio)
-        # store the result in a txt file
+
+         # store the result in a txt file
         if str is bytes:
             result = u"{}".format(txt).encode("utf-8")
         else:
@@ -17,7 +23,11 @@ def recognize_speech(audio_input,txt_output):
 
         with open(txt_output, "a") as f:
             f.write(result)
-        print(result)
+            print(result)
 
+    except sr.UnknownValueError:
+        print("Google Web Speech Recognition could not understand audio")
+    except sr.RequestError as error:
+        print("Could not request results from Google Web Speech Recognition service; {0}".format(error))
 # test this function
 recognize_speech("C:/Users/taotao/Desktop/research/OSR_us_000_0010_8k.wav","C:/Users/taotao/Desktop/research/OSR_us_000_0010_8k.txt")
