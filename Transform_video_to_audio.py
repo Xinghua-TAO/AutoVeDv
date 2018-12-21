a2f43885b29a8cf11af180ab4c9ba55ee644c241
TAO <44677512+Xinghua-TAO@users.noreply.github.com>
2018-12-21 12:02:01 -0500

    the latest version

--- Transform_video_to_audio.py
+++ Transform_video_to_audio.py
@@ -1,6 +1,7 @@
 FFMPEG_BIN = "C:/ffmpeg/bin/ffmpeg.exe"
 import subprocess as sp
 
+
 # define a new function and its parameters are the path of input and output materials
 def convert_video(video_input, video_output):
     command = [FFMPEG_BIN,
@@ -9,9 +10,11 @@ def convert_video(video_input, video_output):
                "-vn",  # disable video recording
                "-ar", "44100",  # sets the sampling rate for audio streams
                "-ac", "2",  # set the number of audio channels
-               "-b:a", "1640k",  # set the audio bit
+               "-acodec", "pcm_s16le",
+               "-b:a", "128k",  # set the audio bit
                video_output]  # output path
     sp.Popen(command)
 
+
 # test new function
-convert_video("C:/Users/taotao/Desktop/research/test.mp4", "C:/Users/taotao/Desktop/research/test.wav")
+convert_video("C:/Users/taotao/Desktop/research/test/HHcut.mp4", "C:/Users/taotao/Desktop/research/test/HHcut.wav")
