1f13a3d641dd2af928ffda3960a7233b15b2739e
TAO <44677512+Xinghua-TAO@users.noreply.github.com>
2019-01-30 14:16:24 -0500

    Add files via upload

--- SpeechRecognition_GoogleCloud_Plus.py
+++ SpeechRecognition_GoogleCloud_Plus.py
@@ -1,8 +1,8 @@
-import string
+
 import numpy
 
 
-def transcribe_gcs_with_word_time_offsets(speech_file, csv_output):
+def transcribe_gcs_with_word_time_offsets(speech_file, csv_file):
     import io
     """Transcribe the given audio file."""
     from google.cloud import speech
@@ -28,7 +28,7 @@ def transcribe_gcs_with_word_time_offsets(speech_file, csv_output):
 
     for result in result.results:
         alternative = result.alternatives[ 0 ]
-        words=[]
+        words = [ ]
 
         for word_info in alternative.words:
             word = word_info.word
@@ -37,26 +37,27 @@ def transcribe_gcs_with_word_time_offsets(speech_file, csv_output):
 
             trans = "{}, {}, {}".format(
                 word,
-                start_time.seconds*1000 + start_time.nanos*1e-6,
-                end_time.seconds*1000 + end_time.nanos*1e-6)
+                start_time.seconds * 1000 + start_time.nanos * 1e-6,
+                end_time.seconds * 1000 + end_time.nanos * 1e-6)
 
             trans_list = trans.split(",")
             words = words + trans_list
 
-        row_number = int(len(words)/3)
+        row_number = int(len(words) / 3)
         mylist = numpy.array(words).reshape(row_number, 3)
 
         # change the list we get to a table and add a head to it
-        list_headline = ['word', 'onset', 'offset']
+        list_headline = [ 'word', 'onset', 'offset' ]
         table = pd.DataFrame(columns=list_headline, data=mylist)
 
-
         # to fit this table with datavyu, we need to change the order of the columns
-        table = table[['onset', 'offset', 'word']]
+        table = table[ [ 'onset', 'offset', 'word' ] ]
         print(table)
 
         # store the table in a csv file
-        table.to_csv(csv_output)
+        table.to_csv(csv_file)
 
 
-transcribe_gcs_with_word_time_offsets("C:/Users/taotao/Desktop/research/test/HHcut.wav","C:/Users/taotao/Desktop/research/test/HHcut.csv")
+# transcribe_gcs_with_word_time_offsets("C:/Users/taotao/Desktop/research/test/HHcut.wav",
+                                     # "C:/Users/taotao/Desktop/research/test/HHcut.csv")
+
